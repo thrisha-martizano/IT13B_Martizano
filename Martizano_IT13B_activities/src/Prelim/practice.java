0493c50357c7addbae4ee04d94b126ffb9a77216
thrisha-martizano <martizanothrisha@gmail.com>
2025-03-08 13:50:39 +0800

    java code practice

---
+++ Martizano_IT13B_activities/src/Prelim/practice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ *
+ */
+public class practice {
+    public static void main(String[] args){
+
+    String name = "THRISHA";
+    for (int i = 6; i >= 0; i--) {
+    System.out.print(name.charAt(i) + " ");
+
+    }
+}
+}
